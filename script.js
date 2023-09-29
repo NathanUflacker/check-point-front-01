659359a97cb76fcf75e56c7850c22c2982da4d6f
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-29 11:00:32 -0300

    respostas checkpoint

---
+++ script.js
@@ -0,0 +1,28 @@
+alert("Alô?");
+
+nome = "Nathan"
+idade = 22
+let palmeirense = true
+let valorProduto = 10.50
+listaTimes = ['Flamengo', 'Palmeiras', "Corínthians"]
+
+console.log(nome)
+console.log(idade)
+console.log(palmeirense)
+console.log(valorProduto)
+console.log(listaTimes)
+
+// Operações matemáticas
+
+let soma = 1 + 1
+console.log(soma)
+
+let sub = 2 - 1
+console.log(sub)
+
+let multiplica = 2 * 15
+console.log(multiplica)
+
+let divide = 20 / 2
+console.log(divide)
+
